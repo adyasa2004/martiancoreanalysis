20159d2922b749a0f066860c6f40f37eaf2cb2ae
Adyasa Mohapatra <163669640+adyasa2004@users.noreply.github.com>
2025-06-25 12:52:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/martiancoreanalysis/',  // 👈 REQUIRED for GitHub Pages
+  base: '/martiancoreanalysis/',  
   plugins: [react()],
 });
